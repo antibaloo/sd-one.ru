eae36a88cc74c597efbfdd8430361faab58000fb
Andrey Abalakov <a.s.abalakov@yandex.ru>
2024-12-20 11:05:38 +0300

    Создание заявки по новому api d7, заявка привязывается к установке. Ответственным за заявку становится ответственный за установку.

--- pub/crm/import/index.php
+++ pub/crm/import/index.php
@@ -4,25 +4,48 @@ if (\Bitrix\Main\Loader::includeModule('crm')) {
 	$hd = fopen(__DIR__."/log.txt", "a"); // отладочная запись в лог
 	$arResult = array();
 	$arResult = json_decode(file_get_contents('php://input'), true);
-	$lead = new CCrmLead;
-	$arFields = Array(
-		"TITLE"	=>	$arResult['subject'],
-		"ASSIGNED_BY_ID" => 1,
-		"COMMENTS" => $arResult['message'],
-		"STATUS_ID" => 'NEW',
-		"OPENED" => "Y"
-	);
 	fwrite($hd,"Получен запрос от хоста: ".$_SERVER['REMOTE_ADDR']."\n");
 	fwrite($hd,"Время получения запроса: ".date('H:i:s d-m-Y',$_SERVER['REQUEST_TIME'])." (МСК)\n");
 	fwrite($hd,"Запрос авторизован пользователем: ".$_SERVER['REMOTE_USER']."\n");
 	fwrite($hd, print_r($arResult,1));
-	fwrite($hd, print_r($arFields,1));
-	$leadId = $lead->Add($arFields,true,array('CURRENT_USER'=>1));
-	if ($leadId) fwrite($hd, "Создан лид с идентификатором: ".$leadId."\n");
-	else fwrite($hd, "При создании лида произошла ошибка: ".$lead->LAST_ERROR."\n");
 	fwrite($hd,"-----------------------------------------------------------------\n");
-	if ($arResult['subject'] == '_Связь с установкой восстановлена'){
-		$terminalId = $arResult['id'];
+	
+	// Ищем установку в Б24
+	$terminalId = $arResult['id'];
+    $dealFactory = \Bitrix\Crm\Service\Container::getInstance()->getFactory(\CCrmOwnerType::Deal);
+    $dealParams = ["select" => ["ID", "UF_CRM_LATITUDE","UF_CRM_LONGITUDE",],"filter" =>["CATEGORY_ID" => 1,"UF_CRM_TERMINAL_ID" => $terminalId]];
+    $deals = $dealFactory->getItems($dealParams);
+    if (!$deals){
+		fwrite($hd, "Установка с id терминала ".$terminalId." не найдена в Б24\n");
+		fwrite($hd,"-----------------------------------------------------------------\n");
+		fclose($hd);
+		echo "OK";
+	   	return;
+    }
+	$equipmentId = (int)$deals[0]["ID"];
+	// Ищем ответственного за установку
+	$assignedById = (int)$deals[0]["ASSIGNED_BY_ID"];
+	// Создаем заявку
+	$leadFactory = \Bitrix\Crm\Service\Container::getInstance()->getFactory(\CCrmOwnerType::Lead);
+	$lead = $leadFactory->createItem();
+	$lead
+		->setAssignedById($assignedById)
+		->setTitle($arResult["subject"])
+		->setComments($arResult['message'])
+		->set('UF_CRM_EQUIPMENT', $equipmentId);
+	$context = new \Bitrix\Crm\Service\Context();
+	$context->setUserId(1);
+	$operation = $leadFactory->getAddOperation($lead,$context);
+	$result = $operation->launch();
+	if (!$result->isSuccess()){
+		fwrite($hd, "при создании заявки произошла ошибка:\n");
+		fwrite($hd, print_r($result->getErrorMessages(),1));
+		fwrite($hd,"-----------------------------------------------------------------\n");
+		fclose($hd);
+		return;
+	}
+	// Если это ссобщение о выходе установки на связь
+	if ($arResult['subject'] == 'Связь с установкой восстановлена'){
 		$value = json_decode($arResult["value"], true);
     	$latitude = $value["latitude"];
     	$longitude = $value["longitude"];
@@ -33,25 +56,16 @@ if (\Bitrix\Main\Loader::includeModule('crm')) {
 			echo "OK";
 	    	return;
 		}
-		$entityTypeId = \CCrmOwnerType::Deal;
-    	$factory = \Bitrix\Crm\Service\Container::getInstance()->getFactory($entityTypeId);
-    	$params = ["select" => ["ID", "UF_CRM_LATITUDE","UF_CRM_LONGITUDE",],"filter" =>["CATEGORY_ID" => 1,"UF_CRM_TERMINAL_ID" => $terminalId]];
-    	$items = $factory->getItems($params);
-    	if (!$items){
-			fwrite($hd, "сделки направления 'устрановка' с id терминала ".$terminalId." не найдены\n");
-			fwrite($hd,"-----------------------------------------------------------------\n");
-			fclose($hd);
-			echo "OK";
-	    	return;
-    	}
-		$item = $factory->getItem($items[0]["ID"]);
-    	$item->set('UF_CRM_LATITUDE',$latitude);
-    	$item->set('UF_CRM_LONGITUDE',$longitude);
-    	$operation = $factory->getUpdateOperation($item);
-    	$operation->disableAllChecks();
-    	$result = $operation->launch();
+
+		$deal = $dealFactory->getItem($equipmentId); // Получаем установку по id найденному ранее
+    	$deal->set('UF_CRM_LATITUDE',$latitude);
+    	$deal->set('UF_CRM_LONGITUDE',$longitude);
+    	$dealOperation = $dealFactory->getUpdateOperation($deal);
+    	$dealOperation->disableAllChecks();
+    	$result = $dealOperation->launch();
 		if (!$result->isSuccess()){
-			fwrite($hd, "при записи координат в сделку произошла ошибка: ".$$res->getErrorMessages()."\n");
+			fwrite($hd, "при записи координат в установку произошла ошибка:\n");
+			fwrite($hd, print_r($result->getErrorMessages(),1));
 			fwrite($hd,"-----------------------------------------------------------------\n");
 			fclose($hd);
 			return;
@@ -62,7 +76,7 @@ if (\Bitrix\Main\Loader::includeModule('crm')) {
 				'TEXT' => json_encode($comment),
 				'SETTINGS' => array('HAS_FILES' => 'N'), //cодержит ли файл комментарий
 				'AUTHOR_ID' => 1, //ID пользователя, от которого будет добавлен комментарий
-				'BINDINGS' => array(array('ENTITY_TYPE_ID' =>$entityTypeId, 'ENTITY_ID' => $item->getId())) // привязка к сущности CRM: ENTITY_TYPE_ID - тип сущности CRM (2 - Сделка), 'ENTITY_ID' - ID сделки в системе.
+				'BINDINGS' => array(array('ENTITY_TYPE_ID' =>\CCrmOwnerType::Deal, 'ENTITY_ID' => $deal->getId()))
 			));
 	}
 	fclose($hd);
